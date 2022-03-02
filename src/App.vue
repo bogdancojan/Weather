96f24a1af952055c4747c8422f569686cfe0a8b7
bogdancojan <Bogdan.Cojan@theaccessgroup.com>
2022-03-02 02:28:13 +0200

    Fetch & display the weather

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,82 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container container-main">
+    <input
+      type="text"
+      placeholder="Search..."
+      v-model="query"
+      @keypress="fetchWeather"
+    />
+    <Content :weather="weather" v-if="typeof weather.main != 'undefined'" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Content from "./views/Content.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Content,
+  },
+  data() {
+    return {
+      api_key: "96aa65a040e7ffb4327afcf6c0ecaa55",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
+  },
+  methods: {
+    async fetchWeather(e) {
+      if (e.key == "Enter") {
+        const res = await fetch(
+          this.url_base +
+            "weather?q=" +
+            this.query +
+            "&units=metric&APPID=" +
+            this.api_key
+        );
+
+        this.weather = await res.json();
+        this.query = "";
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Times New Roman", Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: auto;
+  width: 50%;
+}
+
+.container-main {
   margin-top: 60px;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+}
+
+input {
+  width: 40%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  border-radius: 16px;
+  border: none;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+}
+
+input:focus {
+  outline: none;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
---
+++ src/views/Content.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="card mb-3">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <div class="row g-0">
+          <img
+            style="margin: 10px"
+            src="../assets/sun.png"
+            class="img-fluid rounded-start"
+            v-if="weather.weather[0].main == 'Clear'"
+          />
+          <img
+            style="margin: 10px"
+            src="../assets/rain.png"
+            class="img-fluid rounded-start"
+            v-else-if="weather.weather[0].main == 'Rain'"
+          />
+          <img
+            style="margin: 10px"
+            src="../assets/clouds.png"
+            class="img-fluid rounded-start"
+            v-else-if="weather.weather[0].main == 'Clouds'"
+          />
+          <img
+            style="margin: 10px"
+            src="../assets/weather-alert.png"
+            class="img-fluid rounded-start"
+            v-else
+          />
+        </div>
+        <div class="row g-0" style="text-align: center">
+          <h2 style="margin-left: 15px">
+            {{ Math.round(weather.main.temp) }}<span>&deg;</span> C
+          </h2>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div class="card-body" style="margin-top: 60px">
+          <h5 class="card-title">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </h5>
+          <p class="card-text">
+            <b>{{ weather.weather[0].main }}</b>
+          </p>
+          <p class="card-text">
+            <small class="text-muted">{{ dateBuilder() }}</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+  props: {
+    weather: Object,
+  },
+  methods: {
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return day + " " + date + " " + month + " " + year;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h2,
+p,
+h5 {
+  color: #868383;
+  font-size: 24px;
+}
+
+h2 {
+  font-size: 32px;
+  line-height: 50px;
+  font-weight: 900;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 16px;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
